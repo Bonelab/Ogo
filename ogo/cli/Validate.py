d0a2d3ec64515501681b43dba8c77e11202fefbd
Steven Boyd <skboyd@shark.local>
2022-12-15 07:48:29 -0600

    Adding repair functionality

--- ogo/cli/Validate.py
+++ ogo/cli/Validate.py
@@ -37,7 +37,7 @@ def validate(input_image, report_file, print_parts, overwrite, func):
 
     if not (input_image.lower().endswith('.nii') or input_image.lower().endswith('.nii.gz')):
         os.sys.exit('[ERROR] Input must be type NIFTI file: \"{}\"'.format(input_image))
-    
+        
     ogo.message('Reading image: ')
     ogo.message('\"{}\"'.format(input_image))
     ct = sitk.ReadImage(input_image)
@@ -58,25 +58,14 @@ def validate(input_image, report_file, print_parts, overwrite, func):
     report += '  {:>27s} {:.2f}\n'.format('version:', script_version)
     report += '  {:>27s} {:s}\n'.format('creation date:', str(date.today()))
     report += '\n'
-    
-    # Command line
-    cmd_line = ''
-    cmd_line += '  {:>27s}\n'.format('_________________Command line')
-    cmd_line += '\n'
-    cmd_line += '  {}\n'.format('Edit the command below. Replace NL with the new labels.')
-    cmd_line += '\n'
-    #cmd_line += '  {}\n'.format('ogoValidate repair \\')
-    cmd_line += '  {}\n'.format('python ../../code/Ogo/ogo/cli/Validate.py repair \\')
-    cmd_line += '    {} \\\n'.format(input_image)    
-    cmd_line += '    {}_REPAIR{} \\\n'.format(name,ext)
-    cmd_line += '    {}\n'.format('--relabel_parts \\')
-        
+            
     # Gather all labels in image and determine number of parts each label is broken into
     ogo.message('Gather list labels in image and determine number of parts.')
     filt = sitk.LabelShapeStatisticsImageFilter() # Used to get labels in image
     filt.Execute(ct)
     n_labels = filt.GetNumberOfLabels()
     labels = filt.GetLabels()
+    label_repair_list = [] # list of (label, part, new_label)
     
     thres = sitk.BinaryThresholdImageFilter() # Used to isolate labels and find # of parts
     thres.SetInsideValue(1)
@@ -87,7 +76,8 @@ def validate(input_image, report_file, print_parts, overwrite, func):
     
     report += '  {:>27s}\n'.format('___________________Report')
     report += '  {:>27s} {:s}\n'.format('number of labels:',str(n_labels))
-    report += '  {:>27s} {:>8s} {:>6s} {:>10s} {:>22s}\n'.format('LABEL','SIZE','PART','PART_Sz','CENTROID')
+    report += '  {:>27s} {:>8s} {:>6s} {:>10s} {:>22s} {:>6s}\n'.format('LABEL','SIZE','PART','VOL','CENTROID','OFFSET')
+    report += '  {:>27s} {:>8s} {:>6s} {:>10s} {:>22s} {:>6s}\n'.format('#','voxels','#','mm3','(mm,mm,mm)','mm')
         
     for idx,label in enumerate(labels):
         ogo.message('  processing label {} ({})'.format(label,lb.labels_dict[label]['LABEL']))
@@ -108,23 +98,64 @@ def validate(input_image, report_file, print_parts, overwrite, func):
         
         if (print_parts):
             label_fname = '{}_lab{:02}{}'.format(name,label,ext)
-            sitk.WriteImage(ct_conn_sorted, label_fname)
+            sitk.WriteImage(ct_conn_sorted, label_fname) # ERROR: Should write image with original label (replacelabel)
         
         report += '  {:>22s}{:>5s} {:8.0f} '.format('('+desc+')',str(label),size)        
         # Generate report data
         for part in stats.GetLabels(): # for each part of a given label
             c = stats.GetCentroid(part)
-            if part>1:
-                report += '  {:>22s}{:>5s} {:8s} {:6d} {:10.1f} ({:6.1f},{:6.1f},{:6.1f})\n'.format('','','',part,stats.GetPhysicalSize(part),c[0],c[1],c[2])
-            else:
+            if part==1:
+                ref_c = c
                 report += '{:6d} {:10.1f} ({:6.1f},{:6.1f},{:6.1f})\n'.format(part,stats.GetPhysicalSize(part),c[0],c[1],c[2])
+            else:
+                distance_between_centroids = np.sqrt((ref_c[0] - c[0])**2 + (ref_c[1] - c[1])**2 + (ref_c[2] - c[2])**2)
+                report += '  {:>22s}{:>5s} {:8s} {:6d} {:10.1f} ({:6.1f},{:6.1f},{:6.1f}) {:6.1f}\n'\
+                          .format('','','',part,stats.GetPhysicalSize(part),c[0],c[1],c[2],distance_between_centroids)
         
-        # Generate command line
+        # Generate suggestion of new label for command line suggestion
         if n_parts>1 and label <80:
-            cmd_line += '    {:d} \\\n'.format(label)                  # current label
-            for part in stats.GetLabels(): 
-                cmd_line += '      {:d} {:s} \\\n'.format(part,'NL')      # part number and new label
-
+            for part in stats.GetLabels():
+                if part>1:
+                    swap = [label,part,label]
+                    label_repair_list.append(swap)
+    
+    # Command line
+    cmd_line = ''
+    cmd_line += '  {:>27s}\n'.format('_________________Command line')
+    cmd_line += '\n'
+    cmd_line += '  {}\n'.format('Edit the command below. Should replaced label be zero?')
+    cmd_line += '\n'
+    cmd_line += '  {}\n'.format('ogoValidate repair \\')
+    cmd_line += '    {} \\\n'.format(input_image)    
+    cmd_line += '    {}_REPAIR{} \\\n'.format(name,ext)
+    cmd_line += '    {}\n'.format('--relabel_parts \\')
+    for idx,swap in enumerate(label_repair_list):
+        cmd_line += '    '+' '.join('{:d}'.format(i) for i in swap)
+        if idx<len(label_repair_list)-1:
+            cmd_line += ' \\\n'
+        else:
+            cmd_line += '\n'
+             
+    #ogo.message('  [' + ', '.join('{:d}'.format(i) for i in final_labels) + ']')
+    
+    #print('length = ',len(label_repair_list))
+    #count_label = 0
+    #for label, swap in label_repair_dict.items():
+    #    count_label += 1
+    #    count_part = 0
+    #    cmd_line += '    {:d} \\\n'.format(label)              # label
+    #    for part, newlabel in swap.items():
+    #        count_part += 1
+    #        cmd_line += '      {:d} {:d} '.format(part,newlabel)        # part number and new label
+    #        if count_label<len(label_repair_dict) or count_part<len(swap):
+    #            cmd_line += '\\\n'
+    #        else:
+    #            cmd_line += '\n'
+            
+    #for label_repair in label_repair_dict:
+    #    print(label_repair)
+    #    print(label_repair_dict.key(label_repair))
+    #print(label_repair_dict)
     # largest_component_binary_image = sorted_component_image == 1   # gets the component 1 from the image
 
     
@@ -188,7 +219,22 @@ def repair(input_image, output_image, relabel_parts, overwrite, func):
     ogo.message('\"{}\"'.format(input_image))
     ct = sitk.ReadImage(input_image)
     
-    #ogo.message('Done.')
+    if (relabel_parts):
+        if np.remainder(len(relabel_parts),3) != 0:
+            os.sys.exit('[ERROR] Incorrect argement for --relabel_parts. Tuples of 3 expected.')
+        n_relabel_parts = int(len(relabel_parts) / 3)
+        relabel_parts = np.reshape(relabel_parts, (n_relabel_parts,3))
+        ogo.message('Relabelling {:d} parts in image'.format(n_relabel_parts))
+        for swap in relabel_parts:
+            label = swap[0]
+            part = swap[1]
+            new_label = swap[2]
+            ogo.message('  label {:d} ({:s}), part {} --> {} ({})'\
+                        .format(label,lb.labels_dict[label]['LABEL'],part,new_label,lb.labels_dict[new_label]['LABEL']))
+
+    else:
+        ogo.message('No operations requested. Did you mean to set --relabel_parts ?')
+    ogo.message('Done.')
     
     
 def main():
@@ -224,6 +270,28 @@ ogoValidate validate /Users/skboyd/Desktop/ML/test/robust/frag/RETRO_01638_NNUNE
 ogoValidate validate /Users/skboyd/Desktop/ML/test/robust/frag/RETRO_02317_NNUNET.nii.gz
 ogoValidate validate /Users/skboyd/Desktop/ML/test/robust/mixed/RETRO_01964_NNUNET.nii.gz
 
+ogoValidate validate /Users/skboyd/Desktop/ML/test/robust/frag/RETRO_02317_NNUNET.nii.gz
+
+  ogoValidate repair \
+    /Users/skboyd/Desktop/ML/test/robust/frag/RETRO_02317_NNUNET.nii.gz \
+    /Users/skboyd/Desktop/ML/test/robust/frag/RETRO_02317_NNUNET_REPAIR.nii.gz \
+    --relabel_parts \
+    3 2 3 \
+    3 3 3 \
+    3 4 3 \
+    4 2 4 \
+    5 2 5 \
+    5 3 5 \
+    5 4 5 \
+    5 5 5 \
+    5 6 5 \
+    5 7 5 \
+    5 8 5 \
+    5 9 5 \
+    5 10 5 \
+    6 2 6 \
+    6 3 6
+
 '''
 
     # Setup argument parsing
@@ -247,7 +315,7 @@ ogoValidate validate /Users/skboyd/Desktop/ML/test/robust/mixed/RETRO_01964_NNUN
     parser_repair = subparsers.add_parser('repair')
     parser_repair.add_argument('input_image', help='Input image file (*.nii, *.nii.gz)')
     parser_repair.add_argument('output_image', help='Output image file (*.nii, *.nii.gz)')
-    parser_repair.add_argument('--relabel_parts', type=int, nargs='*', default=[0], metavar='LABEL PART NEWLABEL', help='List of labels, parts, and new labels; space separated. Example shows relabelling L4 part 2 to  L3: (e.g. 7 2 8)')
+    parser_repair.add_argument('--relabel_parts', type=int, nargs='*', default=[], metavar='LABEL PART NEWLABEL', help='List of labels, parts, and new labels; space separated. Example shows relabelling L4 part 2 to  L3: (e.g. 7 2 8)')
     #parser_repair.add_argument('--component', type=int, default=0, metavar='#',help='Connected component number (default: %(default)s)')
     #parser_repair.add_argument('--label', type=int, default=0, metavar='#',help='Label of bone (default: %(default)s)')
     parser_repair.add_argument('--overwrite', action='store_true', help='Overwrite output image without asking')
