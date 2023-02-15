7e944b7e2eb2f937578e3ab3f5da84599aa54fee
Bryn Matheson <brynmatheson@bonelabs-iMac-5.local>
2023-02-15 15:44:40 -0700

    adding helper explanations

--- ogo/util/Helper.py
+++ ogo/util/Helper.py
@@ -1493,8 +1493,9 @@ def determine_normality(input_array):
 
     return normality 
 
-def _quartile_predict(hu, voxel_volume, hu_to_mass_attenuation_slope, hu_to_mass_attenuation_intercept, hu_to_density_slope, hu_to_density_intercept, triglyceride_mass_attenuation, K2HPO4_mass_attenuation):
-       
+'''_quartile_predict takes in the specific calibration parameters at the specified quartile values by the user and 
+    converts the image to K2HPO4 based on the calibration parameters at that quartile. '''
+def _quartile_predict(hu, voxel_volume, hu_to_mass_attenuation_slope, hu_to_mass_attenuation_intercept, hu_to_density_slope, hu_to_density_intercept, triglyceride_mass_attenuation, K2HPO4_mass_attenuation):  
         voxel_volume = voxel_volume / 1000.0
 
         # Convert HU to mass attenuation coefficient
@@ -1524,6 +1525,8 @@ def _quartile_predict(hu, voxel_volume, hu_to_mass_attenuation_slope, hu_to_mass
         return k2hpo4
     
 def add_to_filename(filepath, suffix):
+    '''Will add a suffix to the end of a .nii.gz file or .nii file 
+    while maintaining the original path and file'''
     directory, filename = os.path.split(filepath)
     if filename.endswith(".nii.gz"):
         filename = filename[:-7] + suffix
