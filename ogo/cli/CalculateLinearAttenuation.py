b17978ada6f1324564fdf40fa025448d00c32b4a
Steven Boyd <skboyd@ucalgary.ca>
2025-12-03 12:56:52 -0700

    Making progress...

--- ogo/cli/CalculateLinearAttenuation.py
+++ ogo/cli/CalculateLinearAttenuation.py
@@ -15,9 +15,9 @@ import numpy as np
 from ogo.dat.MassAttenuationTables import mass_attenuation_tables
 from ogo.util.echo_arguments import echo_arguments
 import ogo.util.Helper as ogo
-import dect_util as md
+import ogo.util.spectral_util as md
 
-def CalculateLinearAttenuation(energy, mass_concentration, material, ct_number, header, delimiter, quiet):
+def CalculateLinearAttenuation(energy, mass_concentration, material, measured_ctn, header, delimiter, description, quiet):
   
   entry = []
   
@@ -26,65 +26,101 @@ def CalculateLinearAttenuation(energy, mass_concentration, material, ct_number, 
     os.sys.exit('[ERROR] Energy must be between 30 and 200 keV.')
   if mass_concentration<=0:
     os.sys.exit('[ERROR] Mass concentration must be positive.')
-  mass_dens = md.mass_density()[material]
-  if mass_concentration > mass_dens:
-    os.sys.exit('[ERROR] Mass concentration cannot be greater than mass density.')
+  density = md.mass_density()[material]
+  if mass_concentration > density and (not quiet):
+    ogo.message('[WARNING] Mass concentration greater than density not physically possible.')
   
   # Calculate error if a CT value is provided?
-  if ct_number:
+  if measured_ctn is not None:
     calculate_error = True
   else:
     calculate_error = False
     
+  # Look up mass attenuations from NIST of our material and reference material water
+  mass_attenuation_of_material = md.interpolate_mass_attenuation(material,energy)
+  mass_attenuation_of_water = md.interpolate_mass_attenuation('water',energy)
+  
+  mass_density_water = md.mass_density()['water'] # from NIST; rho_water = 1.0 g/cm3
+  mass_concentration_water = md.mass_density()['water'] # from NIST; rho_water = 1.0 g/cm3
+  
+  # Calculate the linear attenuations knowing mass concentration
+  mu_material = mass_attenuation_of_material * mass_concentration
+  mu_water = mass_attenuation_of_water * mass_concentration_water
+  
+  # Calculate the CT number (HU)
+  ctn_material = round(md.mu_to_ct_number(mu_material,mu_water))
+  ctn_water = round(md.mu_to_ct_number(mu_water,mu_water))
+  
+  # Calculate total linear attenuation and CT number
+  mu_final = mu_material + mu_water
+  ctn_final = round(md.mu_to_ct_number(mu_final,mu_water))
+  
   # Get the calculated mu (and CT number) 
-  ma_material = md.interpolate_mass_attenuation(material,energy) # from NIST
-  mu_water = md.interpolate_mass_attenuation('water',energy) * md.mass_density()['water']  # from NIST; rho_water = 1.0 g/cm3, so mu = ma * rho
-  mu_calc = ma_material * mass_concentration + mu_water * md.mass_density()['water'] * 0.97
-  #mu_calc = ma_material * mass_concentration
-  ct_calc = round(md.mu_to_ct_number(mu_calc,mu_water))
+  #mu_final = mass_attenuation_of_material * mass_concentration + mu_water * md.mass_density()['water'] * 0.97
+  #mu_final = mass_attenuation_of_material * mass_concentration
+  #ctn_final = round(md.mu_to_ct_number(mu_final,mu_water))
 
   # Calculate error
-  if ct_number:
-    mu_meas = md.ct_number_to_mu(ct_number,mu_water)
-    mu_delta =  mu_calc - mu_meas
-    mu_error = (mu_delta)/mu_meas * 100.0
-    ct_delta =  ct_calc - ct_number
-  
+  if measured_ctn is not None:
+    measured_mu = md.ct_number_to_mu(measured_ctn,mu_water)
+    mu_delta =  mu_final - measured_mu
+    mu_error = (mu_delta)/measured_mu * 100.0
+    ct_delta =  ctn_final - measured_ctn
+    if measured_ctn != 0:
+      ct_error = (ct_delta)/measured_ctn * 100.0
+    
   # Gather results
   if not quiet:
     ogo.message('Input:')
-    ogo.message('  energy     = {:.4f} [keV]'.format(energy))
-    ogo.message('  mass_concentration  = {:.4f} [g/cm3]'.format(mass_concentration))
-    ogo.message('  material   = {:s}'.format(material))
-    ogo.message('Lookup:')
-    ogo.message('  mass_dens of {:s} = {:.4f} [g/cm3]'.format(material,mass_dens))
-    ogo.message('  mu (water) at {} keV = {:.4f} [/cm]'.format(energy,mu_water))
-    ogo.message('  ma ({:s}) at {} keV = {:.4f} [/cm]'.format(material,energy,ma_material))
-    ogo.message('Calculated:')
-    ogo.message('  mu_calc ({:s})  = {:.4f} [/cm]'.format(material,mu_calc))
-    ogo.message('  ct_calc ({:s})  = {:d} [HU]'.format(material,ct_calc))
+    ogo.message('  {:30s} = {:12.4f} [keV]'.format("energy",energy))
+    ogo.message('  {:30s} = {:12.4f} [g/cm3]'.format("mass_concentration",mass_concentration))
+    ogo.message('  {:30s} = {:>12s}'.format("material",material))
+    ogo.message('Lookup and calculations:')
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mass attenuation of {}'.format(material),mass_attenuation_of_material))
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mass attenuation of {}'.format('water'),mass_attenuation_of_water))
+    ogo.message('  {:30s} = {:12.4f} [g/cm3]'.format('mass density of {}'.format(material),density))
+    ogo.message('  {:30s} = {:12.4f} [g/cm3]'.format('mass density of {}'.format('water'),mass_density_water))
+    ogo.message('  {:30s} = {:12.4f} [g/cm3]'.format('mass concentration of {}'.format(material),mass_concentration))
+    ogo.message('  {:30s} = {:12.4f} [g/cm3]'.format('mass concentration of {}'.format('water'),mass_concentration_water))
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mu of {:s} at {} keV'.format(material,energy),mu_material))
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mu of {:s} at {} keV'.format('water',energy),mu_water))
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('CTn of {:s} at {} keV'.format(material,energy),ctn_material))
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('CTn of {:s} at {} keV'.format('water',energy),ctn_water))
+    ogo.message('Results:')
+    ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mu_final ({:s})'.format(material),mu_final))
+    ogo.message('  {:30s} = {:12d} [HU]'.format('ctn_final ({:s})'.format(material),ctn_final))
     if calculate_error:
       ogo.message('Comparison to observed:')
-      ogo.message('  ct_number   = {:.1f} [HU]'.format(ct_number))
-      ogo.message('  mu_meas   = {:.4f} [/cm]'.format(mu_meas))
-      ogo.message('  mu_delta  = {:.4f} [/cm]'.format(mu_delta))
-      ogo.message('  ct_delta  = {:.4f} [HU]'.format(ct_delta))
-      ogo.message('  mu_error  = {:.4f} %'.format(mu_error))
+      ogo.message('  {:30s} = {:12.1f} [HU]'.format('measured_ctn',measured_ctn))
+      ogo.message('  {:30s} = {:12.4f} [/cm]'.format('converted to mu',measured_mu))
+      ogo.message('  {:30s} = {:12.4f} [/cm]'.format('mu_delta',mu_delta))
+      ogo.message('  {:30s} = {:12.4f} [HU]'.format('ct_delta',ct_delta))
+      ogo.message('  {:30s} = {:12.4f} %'.format('mu_percent_error',mu_error))
+      if measured_ctn != 0:
+        ogo.message('  {:30s} = {:12.4f} %'.format('ct_percent_error',ct_error))
+      else:
+        ogo.message('  {:30s} = {:>12s}'.format('ct_percent_error','n/a'))
+    if description is not None:
+      ogo.message('  {:30s} = {:>12s}'.format('description',description))
   
+  # Primary outcomes only
+  if description is not None:
+    entry.append( ['description', '{:s}'.format(description),'TXT'] )
   entry.append( ['energy', '{:.4f}'.format(energy), '[keV]'] )
   entry.append( ['mass_concentration', '{:.4f}'.format(mass_concentration), '[g/cm3]'] )
   entry.append( ['material', '{:s}'.format(material), '[name]'] )
-  entry.append( ['mass_dens', '{:.4f}'.format(mass_dens), '[g/cm3]'] )
-  entry.append( ['mu_water', '{:.4f}'.format(mu_water), '[/cm]'] )
-  entry.append( ['ma_{:s}'.format(material), '{:.4f}'.format(ma_material), '[/cm]'] )
-  entry.append( ['mu_calc', '{:.4f}'.format(mu_calc), '[/cm]'] )
-  entry.append( ['ct_calc', '{:d}'.format(ct_calc), '[/cm]'] )
+  entry.append( ['mu_final', '{:.4f}'.format(mu_final), '[/cm]'] )
+  entry.append( ['ctn_final', '{:d}'.format(ctn_final), '[/cm]'] )
   if calculate_error:
-    entry.append( ['ct_number', '{:.1f}'.format(ct_number), '[HU]'] )
-    entry.append( ['mu_meas', '{:.4f}'.format(mu_meas), '[/cm]'] )
+    entry.append( ['measured_ctn', '{:.1f}'.format(measured_ctn), '[HU]'] )
+    entry.append( ['measured_mu', '{:.4f}'.format(measured_mu), '[/cm]'] )
     entry.append( ['mu_delta', '{:.4f}'.format(mu_delta), '[/cm]'] )
     entry.append( ['ct_delta', '{:.4f}'.format(ct_delta), '[H]'] )
-    entry.append( ['mu_error', '{:.4f}'.format(mu_error), '[%]'] )  
+    entry.append( ['mu_percent_error', '{:.4f}'.format(mu_error), '[%]'] )  
+    if measured_ctn != 0:
+      entry.append( ['ct_percent_error', '{:.4f}'.format(ct_error), '[%]'] )  
+    else:
+      entry.append( ['ct_percent_error', 'n/a', '[%]'] )  
   
   # Output results for stdout
   if quiet:
@@ -111,7 +147,10 @@ def main():
 '''
   epilog = '''
 Example calls: 
-  python calculate_linear_attenuation.py --kev 50 --mass_concentration 1.0 --material water
+  ogoCalculateLinearAttenuation cha 50.0 0.200
+  ogoCalculateLinearAttenuation cha 100 0.202 --measured_ctn 186
+  ogoCalculateLinearAttenuation water 90 1.0 --measured_ctn 0
+  ogoCalculateLinearAttenuation cha 100 0.202 --measured_ctn 542 -d "PCD_ESP_120kV_080keV_Br40_04Th"
   
 '''
 
@@ -152,7 +191,7 @@ Example calls:
                       default=1.0, 
                       metavar='Concentration', 
                       help='Mass concentration (g/cm3)')
-  parser.add_argument('--ct_number', 
+  parser.add_argument('--measured_ctn', 
                       type=float, 
                       default=None, 
                       metavar='CTN', 
@@ -160,10 +199,14 @@ Example calls:
   parser.add_argument('--header', '-H',
                       action="store_true",
                       help="""Print a header line first (default: %(default)s)""")
-  parser.add_argument ("--delimiter", "-d",
+  parser.add_argument ("--delimiter",
                       metavar='VAL', 
                       default = "\t",
-                      help="""Delimiter character (default: tab, '\\t').""")
+                      help="""Delimiter character (default: tab, '\\t')""")
+  parser.add_argument ("--description", "-d",
+                      metavar='TXT', 
+                      default = None,
+                      help="""Description to pass to output (e.g., filename) (default: %(default)s)""")
   parser.add_argument("--quiet", 
                       action='store_true', 
                       help='Overwrite output without asking')
