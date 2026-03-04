e4c81f8cb0f61e8fef83a94db2385954b84837c3
friedchickean <kdarg29@gmail.com>
2026-03-04 11:19:31 -0700

    Updated outputs to use ogo.Helper to make it consistent with other CLI tools

--- ogo/cli/CheckLabels.py
+++ ogo/cli/CheckLabels.py
@@ -9,6 +9,7 @@ import os
 import sys
 import SimpleITK as sitk
 import numpy as np
+import ogo.util.Helper as ogo
 
 
 """
@@ -24,17 +25,13 @@ def main():
         prog="ogoCheckLabels",
         description="Print labels present in a NIfTI segmentation and optionally validate required labels."
     )
-    parser.add_argument(
-        "input_image", help="Input image file (*.nii, *.nii.gz)")
+    parser.add_argument("input_image", help="Input image file (*.nii, *.nii.gz)")
     parser.add_argument("--require", type=int, nargs="*", default=[],
                         help="Required labels (e.g. --require 2 10)")
     args = parser.parse_args()
 
-    if not os.path.isfile(args.input_image):
-        sys.exit(f'[ERROR] Cannot find file "{args.input_image}"')
-    if not (args.input_image.lower().endswith(".nii") or args.input_image.lower().endswith(".nii.gz")):
-        sys.exit(
-            f'[ERROR] Input must be type NIFTI file: "{args.input_image}"')
+    ogo.pass_check_if_file_exists(args.input_image)
+    ogo.pass_check_file_ending(args.input_image)
 
     ct = sitk.ReadImage(args.input_image, sitk.sitkUInt8)
     arr = sitk.GetArrayViewFromImage(ct)
@@ -42,13 +39,12 @@ def main():
     if args.require:
         missing = [lab for lab in args.require if not np.any(arr == lab)]
         if missing:
-            print("[ERROR] Missing required labels:", missing)
+            ogo.message("[ERROR] Missing required labels: {}".format(missing))
             sys.exit(2)
-        print("[SUCCESS] All required labels are present.")
-        return
+        ogo.message("[SUCCESS] All required labels are present.")
     else:
         labels = sorted(np.unique(arr).tolist())
-        print("Labels found:", labels)
+        ogo.message("Labels found: {}".format(labels))
 
 if __name__ == "__main__":
     main()
